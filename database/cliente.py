a7beb5c2aeb357765e42ec7424590ee5d56f9e1c
Luiz Henrique Ramos <luizgthub@gmail.com>
2025-05-01 23:18:25 -0300

    added fake database and static archive

---
+++ database/cliente.py
@@ -0,0 +1,8 @@
+CLIENTES = [
+  {"id": 1,"nome": "Luiz", "email": "luiz@email.com"},
+  {"id": 2,"nome": "Pedro", "email": "pedro@email.com"},
+  {"id": 3,"nome": "Kelly", "email": "kelly@email.com"},
+  {"id": 4,"nome": "Arthur", "email": "arthur@email.com"},
+  {"id": 5,"nome": "Antony", "email": "antony@email.com"},
+  {"id": 6,"nome": "Carol", "email": "carol@email.com"}
+]
--- routes/cliente.py
+++ routes/cliente.py
@@ -1,10 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from database.cliente import CLIENTES
 
 cliente_route = Blueprint('cliente', __name__)
 
 @cliente_route.route('/')
 def lista_clientes():
- return render_template('lista_clientes.html')
+ return render_template('lista_clientes.html', clientes=CLIENTES)
 
 
 @cliente_route.route('/', methods=['POST'])
